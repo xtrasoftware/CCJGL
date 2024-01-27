7b8b8bf30659d8486354a61b7a8fbd8b45d81555
daunt3d <134453895+daunt2d@users.noreply.github.com>
2024-01-26 23:32:45 -0800

    Create Contents.java

---
+++ dependencies/Contents.java
@@ -0,0 +1,42 @@
+public class Contents
+  {
+  class needs
+    {
+    // Unfinished
+
+
+
+    }
+
+
+   public class Frequents
+      {
+       public void constructArea()
+        {
+         public String xValue = null;
+         public String yValue = null;
+ 
+
+
+        }
+
+
+
+        
+      }
+  
+  public static void main(String[] args)
+    {
+    
+
+
+
+
+    }
+
+
+
+
+
+
+  }
